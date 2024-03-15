1275d7ee0c03b13474d2b33b12dc6e011c1378c7
Danny Walters <dwdanielwalters@gmail.com>
2024-03-15 10:29:53 -0600

    logs/ dir created when logger created, ignore logs/ file in repo

--- app/logger.py
+++ app/logger.py
@@ -1,8 +1,19 @@
 import logging
+import os
 
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                                    '%Y-%m-%d %H:%M:%S')
+
+
 def setup_logging():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    logs_dir = os.path.join(base_dir, 'logs')
+
+    # Create logs directory if it doesn't exist
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
+    # Set up basic configuration for logging
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
